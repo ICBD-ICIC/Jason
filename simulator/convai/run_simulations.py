4e9e5feb673e9f7d180f9331b16f9d599b3d71fb
Stephanie Malvicini <s.a.m_puan@hotmail.com>
2026-05-13 22:39:16 +0200

    remove run with wrong setup

--- simulator/convai/run_simulations.py
+++ simulator/convai/run_simulations.py
@@ -43,7 +43,7 @@ SIM_FOLDERS = [
     #"convai/600/524949443607412737_25pct_credulous",
     ##"convai/600_llm/524949443607412737_25pct_credulous",
     ###"convai/600/524949443607412737_50pct_credulous",
-    #"convai/600_llm/524949443607412737_50pct_credulous",
+    "convai/600_llm/524949443607412737_50pct_credulous",
     #"convai/600/524949443607412737_75pct_credulous",
     #"convai/600_llm/524949443607412737_75pct_credulous",
 ]
