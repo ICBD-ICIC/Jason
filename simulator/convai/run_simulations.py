0d9b1ae4667ca758d7f3bcbdca173c5162531975
Stephanie Malvicini <s.a.m_puan@hotmail.com>
2026-05-13 20:00:00 +0200

    Some results

--- simulator/convai/run_simulations.py
+++ simulator/convai/run_simulations.py
@@ -36,8 +36,10 @@ SIM_FOLDERS = [
     #"convai/600_llm/524949443607412737",
     #"convai/600/524949443607412737_25pct_cautious",
     #"convai/600_llm/524949443607412737_25pct_cautious",
-    "convai/600/524949443607412737_50pct_cautious",
-    "convai/600_llm/524949443607412737_50pct_cautious",
+    #"convai/600/524949443607412737_50pct_cautious",
+    #"convai/600_llm/524949443607412737_50pct_cautious",
+    "convai/600/524949443607412737_75pct_cautious",
+    "convai/600_llm/524949443607412737_75pct_cautious",
 ]
 
 
